2cebd10ed2649c1ca9db1c11110e72eada55c94d
darnissan <67154616+darnissan@users.noreply.github.com>
2022-06-19 16:25:09 +0300

    fixing validation checks on merchant

--- Cards/Merchant.cpp
+++ Cards/Merchant.cpp
@@ -1,7 +1,7 @@
 #include "Merchant.h"
 #include "../utilities.h"
 #include "Card.h"
-void Merchant::print(std::ostream& os) const
+void Merchant::print(std::ostream &os) const
 {
     printCardDetails(os, m_cardName);
     printEndOfCardDetails(os);
@@ -13,13 +13,23 @@ Merchant::Merchant()
     m_cardName = "Merchant";
 }
 
-void Merchant::uniqeAction(std::unique_ptr<Player>& player)
+void Merchant::uniqeAction(std::unique_ptr<Player> &player)
 {
     int userChoice;
     std::string m_input;
     int playerCoins = player->getCoins();
     printMerchantInitialMessageForInteractiveEncounter(std::cout, player->getName(), playerCoins);
-    std::getline(std::cin, m_input);
+    // read input from user and check if number
+    bool validInput = false;
+    bool intInRange = false;
+    do
+    {
+        std::cin >> m_input;
+        userChoice = IsValidInput(m_input);
+        intInRange = IsIntInRange(userChoice);
+    } while (intInRange == false);
+
+    /*std::getline(std::cin, m_input);
     try {
         userChoice = std::stoi(m_input);
     } catch (std::invalid_argument& e) {
@@ -29,33 +39,69 @@ void Merchant::uniqeAction(std::unique_ptr<Player>& player)
         printInvalidInput();
         uniqeAction(player);
     }
-    if (userChoice == 0) {
+    */
+    if (userChoice == 0)
+    {
         printMerchantSummary(std::cout, player->getName(), 0, 0);
     }
-    if (userChoice == 1) {
+    if (userChoice == 1)
+    {
         {
 
-            if (playerCoins >= 5) {
+            if (playerCoins >= 5)
+            {
                 player->heal(1);
                 player->pay(5);
                 printMerchantSummary(std::cout, player->getName(), 1, 5);
-            } else {
+            }
+            else
+            {
                 printMerchantInsufficientCoins(std::cout);
                 printMerchantSummary(std::cout, player->getName(), 1, 0);
             }
         }
-    } else if (userChoice == 2) {
-        if (playerCoins >= 10) {
+    }
+    else if (userChoice == 2)
+    {
+        if (playerCoins >= 10)
+        {
             player->pay(10);
             player->buff(1);
             printMerchantSummary(std::cout, player->getName(), 2, 10);
-        } else {
+        }
+        else
+        {
             printMerchantInsufficientCoins(std::cout);
             printMerchantSummary(std::cout, player->getName(), 2, 0);
         }
     }
 }
-Card* Merchant::clone( ) const
+Card *Merchant::clone() const
 {
     return new Merchant();
+}
+
+int Merchant::IsValidInput(std::string input)
+{
+    int userChoice;
+    try
+    {
+        userChoice = std::stoi(input);
+    }
+    catch (std::invalid_argument)
+    {
+        printInvalidInput();
+        std::cin >> input;
+        IsValidInput(input);
+    }
+    return userChoice;
+}
+
+bool Merchant::IsIntInRange(int num)
+{
+    if (num < 0 || num > 2)
+    {
+        printInvalidInput();
+        return false;
+    }
 }
--- Cards/Merchant.h
+++ Cards/Merchant.h
@@ -5,18 +5,21 @@
 #include <iostream>
 #include <stdlib.h>
 #include <string>
-class Merchant : public Card {
+class Merchant : public Card
+{
 protected:
-    virtual void print(std::ostream& os) const override;
+    virtual void print(std::ostream &os) const override;
 
 private:
+    int IsValidInput(std::string input);
+    bool IsIntInRange(int num);
     std::string m_cardName;
 
 public:
-Card* clone() const  ;
-    void uniqeAction(std::unique_ptr<Player>& player) override;
-    Merchant() ;
-    ~Merchant() {};
+    Card *clone() const;
+    void uniqeAction(std::unique_ptr<Player> &player) override;
+    Merchant();
+    ~Merchant(){};
 };
 
 #endif /* MERCHANT_H_ */
